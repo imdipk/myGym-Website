d3b3a110122f03911cfa69fe5bb38be2246e66c1
Deepak Jangir <jangirdeepak121@gmail.com>
2021-09-12 18:40:19 +0530

    ejs layouts added

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const http = require('http');
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 8000;
+const port = 80;
 const bodyparse = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/contactGym', { useNewUrlParser: true });
+const layouts = require('express-ejs-layouts')
 
 const contactSchema = new mongoose.Schema({
   name: String,
@@ -15,26 +15,24 @@ const contactSchema = new mongoose.Schema({
 });
 const Contact = mongoose.model('Contact', contactSchema);
 
-app.use('/static', express.static('static')) // For serving static files
+app.use(layouts)
+app.use('/static', express.static('static'))
 app.use(express.urlencoded())
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views')) // Set the views directory
+app.set('views', path.join(__dirname, 'views'))
 
-// ENDPOINTS
 app.get('/', (req, res) => {
-  
+
   res.render('home.ejs');
 })
 app.get('/about', (req, res) => {
   res.render('about.ejs');
 })
 app.get('/services', (req, res) => {
-
   res.render('services.ejs');
 })
 app.get('/contact', (req, res) => {
-
   res.render('contact.ejs');
 })
 app.get('/signin', (req, res) => {
@@ -50,7 +48,6 @@ app.post('/contact', (req, res) => {
   })
 })
 
-// START THE SERVER
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- static/home_script.js
+++
@@ -1,4 +0,0 @@
-let joinBtn = document.getElementById('joinBtn')
-joinBtn.addEventListener('click', ()=>{
-    window.location = "http://localhost:8000/signin"
-})
